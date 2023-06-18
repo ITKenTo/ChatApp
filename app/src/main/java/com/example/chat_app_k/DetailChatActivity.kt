e90d58f5f81654a273865ab4c702e7cbabe6cabd
ITKenTo <monsterlt1105@gmail.com>
2023-06-18 16:25:44 +0700

    degin_layout_main

---
+++ app/src/main/java/com/example/chat_app_k/DetailChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chat_app_k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_chat)
+    }
+}
