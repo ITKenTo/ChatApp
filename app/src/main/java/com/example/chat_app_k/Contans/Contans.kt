aa4e6216ab93eff41e9776766d3075076acc2eef
ITKenTo <monsterlt1105@gmail.com>
2023-07-30 15:33:29 +0700

    cloud message

--- app/src/main/java/com/example/chat_app_k/Contans/Contans.kt
+++ app/src/main/java/com/example/chat_app_k/Contans/Contans.kt
@@ -3,7 +3,7 @@ package com.example.chat_app_k.Contans
 class Contans {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAAJg6ZLg:APA91bFXhDrGiKCAjOZE6qwG2fG7bmLp3OFvw_72E5WMDE4Zi0ZB-v_LLb9ivqdgqFRjxh5LUYK7H2E36xGqnUa0Sf-p_gB5O3Bof8leG4q9V0q2MbUINBAjsNK7Ll5tETJ0M9-epfFJ" // get firebase server key from firebase project setting
+        const val SERVER_KEY = "" // get firebase server key from firebase project setting
         const val CONTENT_TYPE = "application/json"
     }
 }
